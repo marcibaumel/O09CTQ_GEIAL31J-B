232dad5ce68c2888ebf535a943bfc2a7e36be37d
Marthon <marcibaumel@gmail.com>
2021-04-11 18:09:16 +0200

    init

---
+++ LogicalDictionary/O09CTQ_Csharp/LogicalDictionary/IOmdbAPI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalDictionary
+{
+    public interface IOmdbAPI
+    {
+        void GetOmdbData(string Title, string Year);
+
+    }
+}
---
+++ LogicalDictionary/O09CTQ_Csharp/LogicalDictionary/ISortedDictionary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalDictionary
+{
+    public interface ISortedDictionary
+    {
+        void AddNewItem(string Title, string Year);
+
+        void DeleteAll();
+
+        void GetValueByKey(string Title, string Year);
+
+        void ExistingItem(string Title);
+
+        int EveryItem();
+
+        bool YearInDictionary();
+    }
+}
